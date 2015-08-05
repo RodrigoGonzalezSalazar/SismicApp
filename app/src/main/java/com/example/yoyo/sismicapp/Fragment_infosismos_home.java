06710f34f17ec85e7096ef2d3a4a84e1acb3339d
RodrigoGonzalezSalazar <rodrigo.gonzalez.s@ug.uchile.cl>
2015-08-05 18:32:09 -0300

    Inicio EI4001, Comienzo implementacion propuestas: Nueva distribucion de info

--- app/src/main/java/com/example/yoyo/sismicapp/Fragment_infosismos_home.java
+++ app/src/main/java/com/example/yoyo/sismicapp/Fragment_infosismos_home.java
@@ -7,13 +7,19 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.text.InputFilter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Fragment_infosismos_home extends Fragment {
     View rootView, view;
@@ -21,7 +27,27 @@ public class Fragment_infosismos_home extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.infosismos_home, container, false);
+        ListView lv = (ListView) rootView.findViewById(R.id.listView);
+        List<String> completes = Arrays.asList("sup1", "sup2", "sup3");
+        List<String> summary = Arrays.asList("sup11", "sup22", "sup33");
+        ListAdapter_InfoAndPrev adapter = new ListAdapter_InfoAndPrev(getActivity(),"prev",completes, summary);
+        lv.setAdapter(adapter);
 
+        //final TextView prueba = (TextView) rootView.findViewById(R.id.miplan_bienvenida_text);
+        //prueba.setOnClickListener(new View.OnClickListener() {
+        //    int i= 0;
+        //    @Override
+        //    public void onClick(View view) {
+        //        if (i==0) {
+        //        prueba.setText("Hola cambio");
+        //        i=1;}
+        //        else { prueba.setText("Hola cambio otra");
+        //            i=0;}
+        //        Toast.makeText(view.getContext(), "click",
+        //                Toast.LENGTH_SHORT).show();
+
+            //}
+        //});
         return rootView;
 
     }
---
+++ app/src/main/java/com/example/yoyo/sismicapp/ListAdapter_InfoAndPrev.java
@@ -0,0 +1,73 @@
+package com.example.yoyo.sismicapp;
+
+
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.CheckedTextView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+///hacer simplemente un list view con 2 text uno que llame al string de la info y otro con "...", en primera instancia se muestra el
+//text 1 con restriccion de X caracteres + el text "..." visible, al hacer click, se quita la restriccion de caracteres y se pone
+//gone el text "..."
+public class ListAdapter_InfoAndPrev extends BaseAdapter {
+    private final List<String> completes;
+    private final List<String> summaries;
+    private final String type;
+    public LayoutInflater inflater;
+    public Fragment activity;
+
+    public ListAdapter_InfoAndPrev(Activity act, String type, List<String> completes, List<String> summaries){
+        this.completes = completes;
+        this.summaries= summaries;
+        this.type = type;
+        inflater = act.getLayoutInflater();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.listinfoandprev_row, null);
+        }
+        TextView text = (TextView) convertView.findViewById(R.id.textView6);
+        ImageView icon = (ImageView) convertView.findViewById(R.id.typeicon);
+        text.setText(completes.get(position));
+        if (position==0){icon.setBackgroundResource(R.drawable.information);;}
+        else if (position==1){
+            if (type.equals("info")){icon.setBackgroundResource(R.drawable.summary);}
+            else if (type.equals("prev")){icon.setBackgroundResource(R.drawable.whattodo);}
+        }
+        else if (position==2){icon.setBackgroundResource(R.drawable.fact);}
+
+        return convertView;
+    }
+
+    @Override
+    public int getCount() {
+        return completes.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return completes.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+}
